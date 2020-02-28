f3908d2fc755ad40010a111826865f1cd5fdf324
harikrishnan <harikrishnan.a@infibeam.net>
2020-02-29 00:18:11 +0530

    guess number

---
+++ 374.guess-number-higher-or-lower.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=374 lang=python3
+#
+# [374] Guess Number Higher or Lower
+#
+# https://leetcode.com/problems/guess-number-higher-or-lower/description/
+#
+# algorithms
+# Easy (41.27%)
+# Likes:    321
+# Dislikes: 1496
+# Total Accepted:    134.5K
+# Total Submissions: 324.7K
+# Testcase Example:  '10\n6'
+#
+# We are playing the Guess Game. The game is as follows:
+# 
+# I pick a number from 1 to n. You have to guess which number I picked.
+# 
+# Every time you guess wrong, I'll tell you whether the number is higher or
+# lower.
+# 
+# You call a pre-defined API guess(int num) which returns 3 possible results
+# (-1, 1, or 0):
+# 
+# 
+# -1 : My number is lower
+# ⁠1 : My number is higher
+# ⁠0 : Congrats! You got it!
+# 
+# 
+# Example :
+# 
+# 
+# 
+# Input: n = 10, pick = 6
+# Output: 6
+# 
+# 
+# 
+#
+
+# @lc code=start
+# The guess API is already defined for you.
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        start, end = 1,n
+        while start <= end:
+            mid = (start + end)//2
+            curr_guess = guess(mid)
+            # print("mid", mid, "guess", curr_guess)
+            if curr_guess < 0:
+                end = mid - 1
+            elif curr_guess > 0:
+                start = mid + 1
+            else:
+                return mid
+        
+# @lc code=end
+
